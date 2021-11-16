51dbb7d55e643f72986f68582c8770a8d118b74a
anurak18844 <anurak18844@gmail.com>
2021-11-16 17:24:00 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,58 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'firsAngular';
+  user = {id:1001,name:"Anurak",address:"Thailand"}
+  i: number = 0;
+  nameInput: string ="";
+  sayHi(){
+    return "Hi from" + this.user.name;
+  } 
+  addCount(){
+    this.i++;
+  }
+
+  deleteCount(){
+    this.i--;
+  }
+
+  readName(name: string){
+    console.log(name);
+    this.nameInput = name;
+  }
+
+  valid: boolean = 1<2;
+  validAge: boolean = false;
+
+  readAge(age: string){
+    let a = parseInt(age);
+    this.validAge = a>=18 && a<=25;
+  }
+
+  menus = ["SHABU","KFC","MK","BBQ","SALAD"]
+
+  courses = [
+    {
+      id: 1,
+      title: "TypeScript"
+    },
+    {
+      id: 2,
+      title: "Java"
+    },
+    {
+      id: 3,
+      title: "PHP"
+    },
+    {
+      id: 4,
+      title: "Angular"
+    },
+    {
+      id: 5,
+      title: "Flutter"
+    }
+  ]
+
 }
